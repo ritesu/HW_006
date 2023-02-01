fe32d0d73adbd3f083b842cac9f89599223b1d37
ritesu <ritesu@mail.ru>
2023-02-02 06:55:11 +1100

    Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите количество строк и столбцов, соответственно");
+int a = int.Parse(Console.ReadLine()?? "0");
+int b = int.Parse(Console.ReadLine()?? "0");
+
+void FillMass(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+           numbers[i, j] = i + j;
+        }
+    }
+}
+
+
+void PrintMass(int[,] numbers)
+{
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+            Console.Write(numbers[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] numbers = new int[a, b];
+FillMass(numbers);
+PrintMass(numbers);
